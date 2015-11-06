35147eba267e6a210a477b8a29a8132de61aac75
Jerry Lin <jerry@connectifier.com>
2015-11-06 15:05:31 -0800

    Tad bit extension in packages for experimentation & some cleanup

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 var app = require('app');  // Module to control application life.
 var BrowserWindow = require('browser-window');  // Module to create native browser window.
 
@@ -12,19 +14,20 @@ var mainWindow = null;
 app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  // TODO(jerry): Add this back to keep app alive after closing windows
+  //if (process.platform != 'darwin') {
     app.quit();
-  }
+  //}
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1280, height: 800});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/viewComponents/index.html');
 
   // Open the DevTools.
   mainWindow.openDevTools();
